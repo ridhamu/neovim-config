fa17379776dbe6f75dce3535ab905bf39ff2d990
ridhamu <ridhauwu@gmail.com>
2025-05-31 18:10:54 +0700

    Initial config of my neovim-config

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,11 @@
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+
+-- register another keymap here(because leader already " ")
+vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>", { desc = "Toggle Neo-tree" })
---
+++ lua/plugins/tokyonight.lua
@@ -0,0 +1,13 @@
+return { 
+  "folke/tokyonight.nvim", 
+  lazy = false, 
+  priority = 1000, 
+  opts = {
+    style = "night", -- "storm"/"day"
+    transparent = false
+  }, 
+  config = function()
+    require("tokyonight").setup(opts) 
+    vim.cmd.colorscheme("tokyonight"); 
+  end
+}
